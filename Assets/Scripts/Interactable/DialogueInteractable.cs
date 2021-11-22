c38252313db482868c461eef014f0ad9dc74db16
Yeis <jcrockyeis@gmail.com>
2021-11-22 12:47:41 -0800

    Adding all dialogues and updating lighthouse prefab

---
+++ Assets/Scripts/Interactable/DialogueInteractable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DreamTeam.Lighthouse.Core.Events;
+
+public class DialogueInteractable : MonoBehaviour, Interactable
+{ 
+    public DialogScriptableObject dialog;
+    private EventController eventController;
+
+    void Start()
+    {
+        eventController = GameObject.FindGameObjectWithTag("EventController").GetComponent<EventController>();
+    }
+
+    public void InteractWith()
+    {
+        Debug.Log("Interacting with Note");
+        eventController.AddEvent(new DialogueEvent(dialog));
+    }
+}
