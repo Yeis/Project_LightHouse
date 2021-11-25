67ded3d149101b1eacf9b2d21088cb7b1dcd6de9
Yeis <jcrockyeis@gmail.com>
2021-11-24 16:57:54 -0800

    Adding Game Boundaries and UI FadeIn/FadeOut animator

--- Assets/Scripts/Dialog/UIController.cs
+++ Assets/Scripts/Dialog/UIController.cs
@@ -8,12 +8,27 @@ public class UIController : MonoBehaviour
 {
     public TextMeshProUGUI textDisplay;
 
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void DisplayDialog(DialogScriptableObject dialog) {
         textDisplay.fontSize = dialog.fontSize;
         textDisplay.font = dialog.fontAsset;
         StartCoroutine(Type(dialog));
         //Clear up after text
 
+    }
+
+    public void FadeIn() {
+        animator.SetTrigger("FadeIn");
+    }
+
+    public void FadeOut() {
+        animator.SetTrigger("FadeOut");
     }
 
     IEnumerator Type(DialogScriptableObject dialog)
